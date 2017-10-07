a1a6c7930f329357aa8ab5b9d6ba569e85737e15
Henry Popp <henry@codedge.io>
2017-10-07 17:10:35 -0500

    fix: mix.exs support for elixir ~> 1.4

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Mapi.Mixfile do
     [
       app: :mapi,
       version: "0.1.0",
-      elixir: "~> 1.5",
+      elixir: "~> 1.4",
       start_permanent: Mix.env == :prod,
       test_coverage: [tool: ExCoveralls],
       preferred_cli_env: [
